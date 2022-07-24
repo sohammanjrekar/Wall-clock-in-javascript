347d0b89b62e178333cf8081dad33afd773e9e2d
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-07-24 23:08:00 +0530

    update

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function updateclock() {
     let min=currenttime.getMinutes();
     let sec=currenttime.getSeconds();
     let timeofday=(hour<12)?"AM":"PM";
-    
+let main=`<p class="unit" id="hours">${hour}</p>
+<p class="unit" id="minutes">${min}</p>
+<p class="unit" id="seconds">${sec}</p>
+<p class="unit" id="ampm">${timeofday}</p>`
+    document.getElementById("main").innerHTML=main;
+
 }
